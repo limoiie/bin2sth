8e0dd9124f54eb9880e9030714e26ca732432e70
gengwangli <limo.iie4@gmail.com>
2020-04-30 16:30:08 +0800

    fix: Correct test case for joint

--- tests/database_test.py
+++ tests/database_test.py
@@ -12,17 +12,17 @@ class TestJointMethods(unittest.TestCase):
             [1, 3], [1, 4], [1, 5],
             [2, 3], [2, 4], [2, 5]
         ]
-        xy_ = joint(x, y)
+        xy_ = list(joint(x, y, c=True))
         self.assertEqual(xy, xy_)
 
     def test_flat(self):
         x = [1, 2]
         y = [[3, 4, 5], [6, 7]]
         xy = [
-            [1, 3], [1, 4], [1, 5],
-            [2, 6], [2, 7]
+            (1, 3), (1, 4), (1, 5),
+            (2, 6), (2, 7)    
         ]
-        xy_ = flat(x, y)
+        xy_ = list(flat(x, y))
         self.assertEqual(xy, xy_)
 
 
