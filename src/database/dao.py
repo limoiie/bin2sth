edd52c4979f70feb3d84e6e3d36110bfd4bd6d6b
gengwangli <limo.iie4@gmail.com>
2020-05-17 21:07:36 +0800

    refactor: Move most functions into DAO from subcls

--- src/database/dao.py
+++ src/database/dao.py
@@ -1,16 +1,75 @@
 import json
 
 from gridfs import GridFS
+from pymongo.collection import Collection
+from pymongo.database import Database
 
 encoding = 'utf-8'
 
 
 class Dao:
-    def __init__(self, db, fs: GridFS):
-        self.db, self.fs = db, fs
+    def __init__(self, db: Database, fs: GridFS, col: Collection, ele_cls):
+        self.db, self.fs, self.col = db, fs, col
+        self.EleCls = ele_cls
 
-    def _put_into_fs(self, obj):
+    def put_into_fs(self, obj):
         return self.fs.put(json.dumps(obj), encoding=encoding)
 
-    def _get_from_fs(self, f_id):
+    def get_from_fs(self, f_id):
         return json.loads(self.fs.get(f_id).read().decode(encoding=encoding))
+
+    def find(self, filtor):
+        return map(Adapter.unwrap_(self), self.col.find(filtor))
+
+    def find_one(self, filtor):
+        return Adapter.unwrap_(self)(self.col.find_one(filtor))
+
+    def id(self, filtor):
+        return map(lambda o: o['_id'], self.col.find(filtor))
+
+    def id_one(self, filtor):
+        return self.col.find_one(filtor)['_id']
+
+    def store(self, objs):
+        return self.col.insert_many(map(Adapter.wrap_(self), objs))
+
+    def store_one(self, obj):
+        return self.col.insert_one(Adapter.wrap_(self)(obj))
+
+
+class Adapter:
+    __register = {}
+
+    def wrap(self, dao, obj):
+        raise NotImplementedError()
+
+    def unwrap(self, dao, js):
+        raise NotImplementedError()
+
+    @staticmethod
+    def register(raw_cls):
+        def f(cls):
+            if raw_cls in Adapter.__register:
+                raise ValueError(F'{raw_cls} has been registered already!')
+            Adapter.__register[raw_cls] = cls()
+        return f
+
+    @staticmethod
+    def wrap_(dao):
+        def f(obj):
+            Cls = type(obj)
+            if Cls not in Adapter.__register:
+                raise ValueError(F'Failed to wrap: {Cls} is not registered!')
+            adapter = Adapter.__register[Cls]
+            return adapter.wrap(dao, obj)
+        return f
+
+    @staticmethod
+    def unwrap_(dao):
+        def f(js):
+            Cls = dao.EleCls
+            if Cls not in Adapter.__register:
+                raise ValueError(F'Failed to unwrap: {Cls} is not registered!')
+            adapter = Adapter.__register[Cls]
+            return adapter.unwrap(dao, js)
+        return f
--- src/database/program_dao.py
+++ src/database/program_dao.py
@@ -1,43 +1,29 @@
 from gridfs import GridFS
 
-from src.database.dao import Dao
+from src.database.dao import Dao, Adapter
 from src.ida.as_json import AsJson
 from src.ida.code_elements import Program
 from src.training.train_args import BinArgs
 from src.utils.collection_op import strip_dict
 
 
+@Adapter.register(Program)
+class ProgramAdapter(Adapter):
+    def wrap(self, dao, prog: Program):
+        dic = AsJson.to_dict(prog)
+        dic['funcs'] = dao.put_into_fs(dic['funcs'])
+        dic['cg'] = dao.put_into_fs(dic['cg'])
+        return dic
+
+    def unwrap(self, dao, dic) -> Program:
+        dic['funcs'] = dao.get_from_fs(dic['funcs'])
+        dic['cg'] = dao.get_from_fs(dic['cg'])
+        return AsJson.from_dict(Program, dic)
+
+
 class ProgramDAO(Dao):
     def __init__(self, db, fs: GridFS):
-        super().__init__(db, fs)
-
-    def store(self, prog: Program):
-        """
-        Store program into db. The functions and callgraphs
-        therein should be stored into GridFS separatively
-        """
-        self.db.binaries.insert_one(self.__prog2dict(prog))
-
-    def find(self, **args):
-        the_filter = make_prog_filter(**args)
-        prog = self.db.binaries.find(the_filter)
-        return map(self.__dict2prog, prog)
-
-    def find_one(self, **args):
-        the_filter = make_prog_filter(**args)
-        prog = self.db.binaries.find_one(the_filter)
-        return self.__dict2prog(prog)
-
-    def __prog2dict(self, prog):
-        prog = prog.dict()
-        prog['funcs'] = self._put_into_fs(prog['funcs'])
-        prog['cg'] = self._put_into_fs(prog['cg'])
-        return prog
-
-    def __dict2prog(self, prog):
-        prog['funcs'] = self._get_from_fs(prog['funcs'])
-        prog['cg'] = self._get_from_fs(prog['cg'])
-        return AsJson.from_dict(Program, prog)
+        super().__init__(db, fs, db.binaries, Program)
 
 
 def make_prog_filter(prog=None, prog_ver=None, cc=None, cc_ver=None, arch=None,
@@ -54,9 +40,10 @@ def load_progs_jointly(db, args: BinArgs):
     """
     prog_dao = ProgramDAO(db, GridFS(db))
     for (prog, prog_ver), (cc, cc_ver), arch, opt, obf in args.joint():
-        ps = prog_dao.find(prog=prog, prog_ver=prog_ver,
-                           cc=cc, cc_ver=cc_ver,
-                           arch=arch, opt=opt, obf=obf)
+        filtor = make_prog_filter(
+            prog=prog, prog_ver=prog_ver, cc=cc, cc_ver=cc_ver,
+            arch=arch, opt=opt, obf=obf)
+        ps = prog_dao.find(filtor)
         if ps is None:
             raise ValueError(f'No such Program info in the database: \
                 prog={prog}, prog_ver={prog_ver}, cc={cc}, cc_ver={cc_ver}, \
